ba5d5143b24bc85e4d0e0e324761341e3c79f20d
Matt Stein <matt@pixelandtonic.com>
2022-01-24 15:55:08 -0800

    Remove `useProjectConfigFile` setting.

--- config/general.php
+++ config/general.php
@@ -23,10 +23,6 @@ return [
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
 
-        // Whether to save the project config out to config/project.yaml
-        // (see https://docs.craftcms.com/v3/project-config.html)
-        'useProjectConfigFile' => true,
-
         'defaultSearchTermOptions' => [
             'subLeft' => true,
             'subRight' => true,
