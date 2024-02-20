d1ac837c7dfb1d2319ba851b3395896017ca00fc
August Miller <hello@gusmiller.com>
2024-02-20 11:47:49 -0800

    Update to Craft 5, switch to CKEditor and Twigpack

---
+++ config/twigpack.php
@@ -0,0 +1,49 @@
+<?php
+
+use craft\helpers\App;
+
+return [
+    // Global settings
+    '*' => [
+        // If `devMode` is on, use webpack-dev-server to all for HMR (hot module reloading)
+        'useDevServer' => false,
+        // Enforce Absolute URLs on includes
+        'useAbsoluteUrl' => true,
+        // The JavaScript entry from the manifest.json to inject on Twig error pages
+        // This can be a string or an array of strings
+        'errorEntry' => '',
+        // String to be appended to the cache key
+        'cacheKeySuffix' => '',
+        // Manifest file names
+        'manifest' => [
+            'legacy' => 'mix-manifest.json',
+            'modern' => 'mix-manifest.json',
+        ],
+        // Public server config
+        'server' => [
+            'manifestPath' => '@webroot/assets/dist',
+            'publicPath' => '@web/assets/dist',
+        ],
+        // webpack-dev-server config
+        'devServer' => [
+            'manifestPath' => App::env('TWIGPACK_MANIFEST_PATH'),
+            'publicPath' => App::env('TWIGPACK_PUBLIC_PATH'),
+        ],
+        // Bundle to use with the webpack-dev-server
+        'devServerBuildType' => 'modern',
+        // Whether to include a Content Security Policy "nonce" for inline
+        // CSS or JavaScript. Valid values are 'header' or 'tag' for how the CSP
+        // should be included. c.f.:
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/script-src#Unsafe_inline_script
+        'cspNonce' => '',
+        // Local files config
+        'localFiles' => [
+            'basePath' => '@webroot/',
+        ],
+    ],
+    // Development environment
+    'dev' => [
+        // If `devMode` is on, use webpack-dev-server to all for HMR (hot module reloading)
+        'useDevServer' => true,
+    ],
+];
