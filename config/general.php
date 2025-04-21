ace5f98ac462c1f26aae8967d5def7bc16bf8ed5
August Miller <hello@gusmiller.com>
2025-04-21 13:35:51 -0700

    Remove aliases

--- config/general.php
+++ config/general.php
@@ -22,8 +22,4 @@ return GeneralConfig::create()
     ->allowAdminChanges(App::env('ALLOW_ADMIN_CHANGES') ?? false)
     // Disallow robots
     ->disallowRobots(App::env('DISALLOW_ROBOTS') ?? false)
-    ->aliases([
-        '@web' => App::env('PRIMARY_SITE_URL'),
-        '@webroot' => '@root/web',
-    ])
 ;
